7e7ef562d5325cb75f6a87b915dacc66a8d3307c
patrick tettenborn <pat@tetten.com>
2019-08-06 16:50:15 +0200

    renaming

---
+++ src/PipeApi.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\patgit\pipeAPI;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * ExtensionName extension class.
+ *
+ * @author Your Name <pat@tetten.com>
+ */
+class pipeAPI extends SimpleExtension
+{
+}
